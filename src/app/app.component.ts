86b1ddaa02f8b301ec95a2c806e0ffc6e2c35811
abdou <002.abdou@gmail.com>
2022-06-08 14:12:47 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { jsPDF } from 'jspdf'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'locationapp';
+  @ViewChild('content', { static: false }) el !: ElementRef
+
+  generatePdf() {
+    let pdf = new jsPDF('p', 'pt', 'a4')
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.output("dataurlnewwindow")
+      }
+    })
+  }
 }
